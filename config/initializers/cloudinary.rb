764e4e1d2b665d308d13269fffe3996a1e4c015d
SNKK62 <koki.seno62@gmail.com>
2021-12-31 22:57:33 +0900

    12/31-16

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = 'dn1ba7rjz'
+    config.api_key = '161855852981934'
+    config.api_secret = '-SDzcAbDolJ_7HMEKNN1h0_XU2w'
+    config.secure = true
+    config.cdn_subdomain = true
+  end
