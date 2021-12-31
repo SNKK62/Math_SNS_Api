6b1546569a4d51540545a15e74a2b9b0839ba21f
SNKK62 <koki.seno62@gmail.com>
2021-12-31 22:09:40 +0900

    12/31-13

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
   # config.hosts << 'mualphatheta.herokuapp.com'
+  config.action_mailer.default_url_options = { host: 'mualphatheta.herokuapp.com'}
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
