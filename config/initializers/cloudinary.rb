f8ed2b98ea4a22b06761d850d957aa3507943190
SNKK62 <koki.seno62@gmail.com>
2022-01-01 17:01:14 +0900

    1/1-7

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 Cloudinary.config do |config|
-    config.cloud_name = 'hfxvwmshp'
-    config.api_key = '429186879321882'
-    config.api_secret = 'y1iUo7umhiEgEnPBYYPaKUILO2I'
+    config.cloud_name = 'hwy7j4ucj'
+    config.api_key = '273915676469567'
+    config.api_secret = 'WiFBcRMT9zEICFYTvFTS04oba2I'
     config.secure = true
     config.cdn_subdomain = true
   end
