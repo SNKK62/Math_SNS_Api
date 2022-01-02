04e2ec63f552e3a6fd8555d83804ea30cccfeac3
SNKK62 <koki.seno62@gmail.com>
2022-01-02 21:31:19 +0900

    1/2-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     after_action :set_csrf_token
+    # protect_from_forgery with: :exception
+    skip_before_filter :verify_authenticity_token
     include ActionController::Cookies
     include SessionsHelper
     include ActionController::RequestForgeryProtection
