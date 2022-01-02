7669d4789a991c3193cfad8851bb7c6d59237bfa
SNKK62 <koki.seno62@gmail.com>
2022-01-02 22:25:18 +0900

    1/2-6

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,12 @@
-if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_math-sns-api', domain: 'mualphatheta.herokuapp.com'
-else
-    Rails.application.config.session_store :cookie_store, key: '_math-sns-api'
-end
+if Rails.env.production?
+
+    # Production では session_store に Redis を利用する
+    Rails.application.config.session_store :redis_store, {
+      servers: ENV['REDIS_URL'],
+      expire_after: 1.week
+    }
+  
+#   else
+#     # Production以外 では session_store に cookie_store(default) を利用する
+#     Rails.application.config.session_store :cookie_store, expire_after: 1.week
+  end
