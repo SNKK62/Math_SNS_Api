3880534e6b0126779ffc16e8f180a51a4387c263
SNKK62 <koki.seno62@gmail.com>
2022-01-02 23:13:46 +0900

    1/2-8

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
 #   skip_before_action :verify_authenticity_token
+skip_before_action :verify_authenticity_token
 
     def create
         user = User.find_by(name: params[:session][:name])
